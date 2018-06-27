c5e312fafb4bb3c4cc48f848e6f63ff54247043f
Matheus Braga <matheusssbraga@gmail.com>
2018-06-27 20:41:12 -0300

    Configuring Thymeleaf as view resolver

--- src/main/java/com/algaworks/brewer/config/WebConfig.java
+++ src/main/java/com/algaworks/brewer/config/WebConfig.java
@@ -1,9 +1,19 @@
 package com.algaworks.brewer.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import com.algaworks.brewer.controller.BeerController;
 
@@ -11,5 +21,36 @@ import com.algaworks.brewer.controller.BeerController;
 @ComponentScan(basePackageClasses = { BeerController.class })
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+	
+	@Autowired
+	private ApplicationContext applicationContext;
+	
+	@Bean
+	public ViewResolver ViewResolver() {
+		ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+		thymeleafViewResolver.setTemplateEngine(templateEngine());
+		thymeleafViewResolver.setCharacterEncoding("UTF-8");
+		
+		return thymeleafViewResolver;
+	}
+	
+	@Bean
+	public ISpringTemplateEngine templateEngine() {
+		SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+		springTemplateEngine.setEnableSpringELCompiler(true);
+		springTemplateEngine.setTemplateResolver(templateResolver());
+		
+		return springTemplateEngine;
+	}
+	
+	private ITemplateResolver templateResolver() {
+		SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
+		springResourceTemplateResolver.setApplicationContext(applicationContext);
+		springResourceTemplateResolver.setPrefix("classpath:/templates/");
+		springResourceTemplateResolver.setSuffix(".html");
+		springResourceTemplateResolver.setTemplateMode(TemplateMode.HTML);
+		
+		return springResourceTemplateResolver;
+	}
 	
 }
--- src/main/java/com/algaworks/brewer/controller/BeerController.java
+++ src/main/java/com/algaworks/brewer/controller/BeerController.java
@@ -1,8 +1,20 @@
 package com.algaworks.brewer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/beer")
 public class BeerController {
 
+	@GetMapping("/register")
+	public ModelAndView beerRegister() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("beer/beer-register");
+		
+		return modelAndView;
+	}
+	
 }
