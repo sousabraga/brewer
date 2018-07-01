071d2a71cc14a3e7598830d98f9c908870f2981d
Matheus Braga <matheusssbraga@gmail.com>
2018-07-01 17:01:59 -0300

    Starting implementation for beer registration

--- src/main/java/com/algaworks/brewer/controller/BeerController.java
+++ src/main/java/com/algaworks/brewer/controller/BeerController.java
@@ -1,19 +1,39 @@
 package com.algaworks.brewer.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.algaworks.brewer.model.Beer;
 
 @Controller
 @RequestMapping("/beer")
 public class BeerController {
 
-	@GetMapping("/register")
-	public ModelAndView beerRegister() {
+	@GetMapping("/new")
+	public ModelAndView newBeer() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("beer/beer-register");
 		
+		return modelAndView;
+	}
+	
+	@PostMapping("/new")
+	public ModelAndView saveBeer(@Valid Beer beer, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			System.err.println("Errors detected. :(");
+		}
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("beer/beer-register");
+		
+		System.out.println(beer);
+	
 		return modelAndView;
 	}
 	
---
+++ src/main/java/com/algaworks/brewer/model/Beer.java
@@ -0,0 +1,73 @@
+package com.algaworks.brewer.model;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+
+public class Beer implements Serializable {
+
+	private static final long serialVersionUID = 5860315063930019183L;
+
+	private Long id;
+	
+	@NotBlank
+	private String sku;
+	
+	@NotBlank 
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("SKU: %s Name: %s", sku, name);
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Beer other = (Beer) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	} 
+	
+}
