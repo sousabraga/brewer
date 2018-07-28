b16f30d655d165fb2f198ab7b2ea5654c9df0e25
Matheus Braga <matheusssbraga@gmail.com>
2018-07-28 18:30:56 -0300

    Starting user's register implementation

---
+++ src/main/java/com/algaworks/brewer/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.algaworks.brewer.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.algaworks.brewer.model.User;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@GetMapping("/new")
+	public ModelAndView newUser(User user, ModelAndView modelAndView) {
+		modelAndView.setViewName("user/user-register");
+		
+		return modelAndView;
+	}
+	
+	@PostMapping("/new")
+	public ModelAndView saveUser(@Valid User user, BindingResult bindingResult, ModelAndView modelAndView, RedirectAttributes redirectAttributes) {		
+		if (bindingResult.hasErrors()) {
+			return newUser(user, modelAndView);
+		}
+		
+		// TODO save the user
+		System.out.println(user);
+
+		redirectAttributes.addFlashAttribute("message", "User successfully saved!");
+		modelAndView.setViewName("redirect:new");
+
+		return modelAndView;
+	}
+
+}
---
+++ src/main/java/com/algaworks/brewer/model/User.java
@@ -0,0 +1,95 @@
+package com.algaworks.brewer.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 8881366462147271108L;
+
+	private Long id;
+	
+	private String name;
+	
+	private LocalDate birth;
+	
+	private String email;
+	
+	private String password;
+	
+	private boolean active;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getBirth() {
+		return birth;
+	}
+
+	public void setBirth(LocalDate birth) {
+		this.birth = birth;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
