dcf41731f6129547af3c2d5e857913ced616c4f8
Matheus Braga <matheusssbraga@gmail.com>
2018-07-28 18:47:18 -0300

    Starting city's register implementation

---
+++ src/main/java/com/algaworks/brewer/controller/CityController.java
@@ -0,0 +1,42 @@
+package com.algaworks.brewer.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.algaworks.brewer.model.City;
+
+@Controller
+@RequestMapping("/city")
+public class CityController {
+	
+
+	@GetMapping("/new")
+	public ModelAndView newCity(City city, ModelAndView modelAndView) {
+		modelAndView.setViewName("city/city-register");
+		
+		return modelAndView;
+	}
+	
+	@PostMapping("/new")
+	public ModelAndView saveCity(@Valid City city, BindingResult bindingResult, ModelAndView modelAndView, RedirectAttributes redirectAttributes) {		
+		if (bindingResult.hasErrors()) {
+			return newCity(city, modelAndView);
+		}
+		
+		// TODO save the customer
+		System.out.println(city);
+
+		redirectAttributes.addFlashAttribute("message", "City successfully saved!");
+		modelAndView.setViewName("redirect:new");
+
+		return modelAndView;
+	}
+
+}
